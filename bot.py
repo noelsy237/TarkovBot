94181c6492cc2ae7b9ba24025523c373dc8ea816
noelsy237 <64825351+noelsy237@users.noreply.github.com>
2024-03-20 22:51:35 +1100

    Updated deafened function

--- bot.py
+++ bot.py
@@ -64,18 +64,33 @@ async def check_camera_status():
                 await output_channel.send(f'{member.display_name} was kicked for not turning on their camera.')
                 print(f'{member.display_name} was kicked for not turning on their camera.')
 
+original_channels = {}
+
 @tasks.loop(seconds=5)    
 async def check_deaf_status():
-    channel_ids = client.get_channel(1185031033364676648), client.get_channel(1191727879743094924), client.get_channel(1218755488150458418), client.get_channel(1007204325887582248), client.get_channel(814857337889488896), client.get_channel(1204190077383745626), client.get_channel(815140823024664586), client.get_channel(838030772577239090), client.get_channel(989690253315022868), client.get_channel(760793358280425492)
-    output_channel = client.get_channel(838386003227836439)
-    loser_channel = client.get_channel(1219591417178099763)
-    for channel in channel_ids:
+    channel_ids = [1185031033364676648, 1191727879743094924, 1218755488150458418, 1007204325887582248, 814857337889488896, 1204190077383745626, 815140823024664586, 838030772577239090, 989690253315022868, 760793358280425492]
+    output_channel_id = 838386003227836439
+    loser_channel_id = 1219591417178099763
+    
+    output_channel = client.get_channel(output_channel_id)
+    loser_channel = client.get_channel(loser_channel_id)
+    
+    for channel_id in channel_ids:
+        channel = client.get_channel(channel_id)
         if channel:
             for member in channel.members:
-                if member.voice.self_deaf:
-                    await member.move_to(loser_channel)
-                    await output_channel.send(f'{member.display_name} was kicked for being a loser.')
-                    print(f'{member.display_name} was kicked for being a loser.')
+                if member.voice and member.voice.channel:
+                    if member.voice.self_deaf and member.id != 253828020412874752:
+                        original_channels[member.id] = member.voice.channel  # Store the original channel
+                        await member.move_to(loser_channel)
+                        await output_channel.send(f'{member.display_name} was kicked for being a loser.')
+
+@client.event
+async def on_voice_state_update(member, before, after):
+    if member.id in original_channels:
+        original_channel = original_channels[member.id]
+        if before.self_deaf and not after.self_deaf:
+            await member.move_to(original_channel)
 
 # Return the price of an item (beta)
 @client.command(aliases=['p'])
